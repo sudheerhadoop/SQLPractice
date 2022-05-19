02932d73f16d43c0e63c825a5721593ba7522bf5
sudheerhadoop <sudhe9630@gmail.com>
2022-05-19 15:27:36 +0530

    Update 511.Game Play Analysis I.sql

--- 511.Game Play Analysis I.sql
+++ 511.Game Play Analysis I.sql
@@ -1,3 +1,21 @@
 select player_id, min(event_date) as first_login 
 from activity 
 group by player_id;
+
+
+----------------
+select 
+player_id,
+first_login
+from (
+    select 
+    player_id, 
+    event_date first_login,
+    dense_rank() over(
+        partition by player_id
+        order by event_date
+    ) poz
+    from Activity
+) src
+where
+poz = 1;
