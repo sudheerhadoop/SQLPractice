991d29d6d4aeb34be598f27c473c8028513f14bd
sudheerhadoop <sudhe9630@gmail.com>
2022-05-19 19:05:22 +0530

    Update 1795. Rearrage Products.sql

--- 1795. Rearrage Products.sql
+++ 1795. Rearrage Products.sql
@@ -5,3 +5,31 @@ select product_id, 'store2' as store, store2 as price from Products
 union
 select product_id, 'store3' as store, store3 as price from Products) t
 where price is not null
+
+
+----------------------------------------------------------------------------
+
+SELECT
+    product_id,
+    'store1' AS store,
+    store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+    
+UNION ALL
+
+SELECT
+    product_id,
+    'store2' AS store,
+    store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+
+UNION ALL
+
+SELECT
+    product_id,
+    'store3' AS store,
+    store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL
