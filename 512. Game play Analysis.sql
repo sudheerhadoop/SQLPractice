0809cbd5deae1bb67ae4cd16c3d453e698675b1e
sudheerhadoop <sudhe9630@gmail.com>
2022-06-06 19:36:38 +0530

    Create 512. Game play Analysis.sql

---
+++ 512. Game play Analysis.sql
@@ -0,0 +1,22 @@
+SELECT DISTINCT player_id, device_id
+FROM Activity
+WHERE (player_id, event_date) in (
+    SELECT player_id, min(event_date)
+    FROM Activity
+    GROUP BY player_id)
+
+#Solution- 2:
+SELECT a.player_id, b.device_id
+FROM
+(SELECT player_id, MIN(event_date) AS event_date FROM Activity
+GROUP BY player_id) a
+JOIN Activity b
+ON a.player_id = b.player_id AND a.event_date = b.event_date;
+
+#Solution- 3:
+SELECT player_id, device_id
+FROM
+(SELECT player_id, device_id, event_date,
+ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS r
+FROM Activity) lookup
+WHERE r = 1;
