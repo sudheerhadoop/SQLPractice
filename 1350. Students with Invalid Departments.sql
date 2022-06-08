9493c0dbbcca2b988c326f8555d0f3aa3dfaa5bd
sudheerhadoop <sudhe9630@gmail.com>
2022-06-08 15:08:11 +0530

    Update 1350. Students with Invalid Departments.sql

--- 1350. Students with Invalid Departments.sql
+++ 1350. Students with Invalid Departments.sql
@@ -2,3 +2,8 @@ select s.id as id, s.name as name from
 students s left join departments d
 on s.department_id=d.id 
 where d.id is null;
+
+
+
+SELECT id, name FROM Students
+WHERE department_id not in (SELECT id from Departments)
